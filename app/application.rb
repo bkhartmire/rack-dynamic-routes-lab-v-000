c28d984282269e52aef6d1abc37b26b8a520ff7e
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-13 09:11:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,2 +1,17 @@
 class Application
   @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|item| item.name = item_name}
+        resp.write item.price
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+    end
+    resp.finish
+  end
+end 
